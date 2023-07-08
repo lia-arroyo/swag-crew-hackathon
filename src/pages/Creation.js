ecba51be38a2d02fe83c1bc072f428a7c79245aa
lia-arroyo <liayzabel@gmail.com>
2023-07-09 10:49:26 +1200

    hovering images shows second img

--- src/pages/Creation.js
+++ src/pages/Creation.js
@@ -1,13 +1,15 @@
 import CasinoIcon from "@mui/icons-material/Casino";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useState } from "react";
-import pet1 from "../images/pets/1/avatar1.png";
+import defaultPet from "../images/pets/1/avatar1.png";
+import petHoverImg from "../images/pets/1/avatar2.png";
 import { Fab, TextField } from "@mui/material";
 import "../styles/creation.css";
 
 const Creation = () => {
   const [pet, setPet] = useState(1);
-  const [petAvatar, setPetAvatar] = useState(pet1);
+  const [petAvatar, setPetAvatar] = useState(defaultPet);
+  const [hoverImage, setHoverImage] = useState(petHoverImg);
 
   const getRandomNum = () => {
     const NUM_OF_PETS = 6;
@@ -24,13 +26,23 @@ const Creation = () => {
     import(`../images/pets/${newPet}/avatar1.png`).then((image) =>
       setPetAvatar(image.default)
     );
+    import(`../images/pets/${newPet}/avatar2.png`).then((image) =>
+      setHoverImage(image.default)
+    );
   };
 
   return (
     <div className="creation">
       <h1>SELECT YOUR PET</h1>
       <TextField id="pet-name" label="Name your pet" variant="outlined" />
-      <img alt="Pet avatar" className="avatar" src={petAvatar} />
+      <div className="avatar">
+        <img alt="Pet avatar" className="normalAvatar" src={petAvatar} />
+        <img
+          alt="Pet avatar doing a pose"
+          className="hoverAvatar"
+          src={hoverImage}
+        />
+      </div>
       <Fab
         color="primary"
         id="random-btn"
