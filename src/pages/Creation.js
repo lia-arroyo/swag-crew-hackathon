b7d6d1b36467d70ace7fc9ba0994583cc484b3fc
lia-arroyo <liayzabel@gmail.com>
2023-07-08 16:00:32 +1200

    randomly choose new pet number from 1 to 6

--- src/pages/Creation.js
+++ src/pages/Creation.js
@@ -1,9 +1,32 @@
+import Button from "@mui/material/Button";
+import CasinoIcon from "@mui/icons-material/Casino";
+import { useState } from "react";
+
 const Creation = () => {
-    return (
-        <div>
-            <h1>Main Screen - Creation</h1>
-        </div>
-    );
+  const [pet, setPet] = useState(1);
+
+  const getNewPet = () => {
+    const newPet = getRandomNum();
+    setPet(newPet === pet ? getRandomNum() : newPet);
+  };
+
+  const getRandomNum = () => {
+    const NUM_OF_PETS = 6;
+    return Math.floor(Math.random() * NUM_OF_PETS) + 1;
+  };
+
+  return (
+    <div>
+      <Button
+        onClick={getNewPet}
+        startIcon={<CasinoIcon />}
+        variant="contained"
+      >
+        Randomise
+      </Button>
+      <h1>Current pet: {pet}</h1>
+    </div>
+  );
 };
 
 export default Creation;
