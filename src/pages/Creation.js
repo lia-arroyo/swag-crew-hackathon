9fcc0d248c74b68ba6b0ab8981365c15ddeb904e
lia-arroyo <liayzabel@gmail.com>
2023-07-08 16:57:06 +1200

    change photo shown depending on the randomly generated pet number

--- src/pages/Creation.js
+++ src/pages/Creation.js
@@ -1,18 +1,27 @@
 import Button from "@mui/material/Button";
 import CasinoIcon from "@mui/icons-material/Casino";
 import { useState } from "react";
+import pet1 from "../images/pets/1.png";
 
 const Creation = () => {
   const [pet, setPet] = useState(1);
-
-  const getNewPet = () => {
-    const newPet = getRandomNum();
-    setPet(newPet === pet ? getRandomNum() : newPet);
-  };
+  const [petAvatar, setPetAvatar] = useState(pet1);
 
   const getRandomNum = () => {
     const NUM_OF_PETS = 6;
     return Math.floor(Math.random() * NUM_OF_PETS) + 1;
+  };
+
+  const getNewPet = () => {
+    let newPet = getRandomNum();
+    while (newPet === pet) {
+      newPet = getRandomNum();
+    }
+
+    setPet(newPet);
+    import(`../images/pets/${newPet}.png`).then((image) =>
+      setPetAvatar(image.default)
+    );
   };
 
   return (
@@ -25,6 +34,7 @@ const Creation = () => {
         Randomise
       </Button>
       <h1>Current pet: {pet}</h1>
+      <img src={petAvatar} alt="Pet avatar" />
     </div>
   );
 };
