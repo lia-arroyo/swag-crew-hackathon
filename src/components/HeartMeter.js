82cf3d93d387832d457d85ef8f19c03df8ec6722
Amisha <amishas171@gmail.com>
2023-07-09 01:24:31 +1200

    Base layout of zoom page implemented, i.e. stats overlay.

--- src/components/HeartMeter.js
+++ src/components/HeartMeter.js
@@ -7,13 +7,15 @@ export default function HeartMeter({ kilometres }) {
   const happiness = 100 - kilometres * 10;
 
   return (
-    <div
-      id="heart-container"
-      style={{ height: happiness, minHeight: 0, maxHeight: 100 }}
-    >
-      {/* "https://www.flaticon.com/free-icons/heart" Pixel Heart icons created by frelayasia - Flaticon */}
-      <img id="red-heart" src={heart} alt="red heart"></img>
-      <img id="empty-heart" src={heart_empty} alt="black heart"></img>
+    <div style={{ height: 80 }}>
+      <div
+        id="heart-container"
+        style={{ height: happiness, minHeight: 0, maxHeight: 100 }}
+      >
+        {/* "https://www.flaticon.com/free-icons/heart" Pixel Heart icons created by frelayasia - Flaticon */}
+        <img id="red-heart" src={heart} alt="red heart"></img>
+        <img id="empty-heart" src={heart_empty} alt="black heart"></img>
+      </div>
     </div>
   );
 }
--- src/pages/Closeup.js
+++ src/pages/Closeup.js
@@ -4,16 +4,29 @@ import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 import NavButton from "../components/NavButton";
 import HeartMeter from "../components/HeartMeter";
 import "../styles/closeup.css";
+import pet from "../images/tan_pet0.png";
 
 const Closeup = () => {
   return (
-    <div>
-      <div className="vert-container">
-        <div className="button-container">
-          <NavButton icon={<MapOutlinedIcon fontSize="large" />}></NavButton>
-          <NavButton icon={<CheckroomIcon fontSize="large" />}></NavButton>
+    <div className="vert-container">
+      <div className="button-container">
+        <NavButton icon={<MapOutlinedIcon fontSize="large" />}></NavButton>
+        <NavButton icon={<CheckroomIcon fontSize="large" />}></NavButton>
+      </div>
+      <div className="status-bar">
+        <div className="left">
+          <img src={pet} alt="pet" className="pet-icon"></img>
+          <div className="name-container">
+            <h3>FLUFFY</h3>
+            <p>Lia's Path Pet</p>
+          </div>
         </div>
-        <HeartMeter kilometres={6} />
+
+        <div className="right">
+          <p>HAPPINESS</p>
+          <HeartMeter kilometres={6} />
+          <p>60%</p>
+        </div>
       </div>
     </div>
   );
