a46eee6e25d43607543d8ad16d69d456987f3c6d
Manav Lal <manavlal5@gmail.com>
2023-07-09 14:35:29 +1200

    Created the modal (not finished to fit theme)

--- src/Components/Distance-Modal.js
+++ src/Components/Distance-Modal.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import TextField from "@mui/material/TextField";
 
 function FindDistance() {
   const [data, setData] = useState([]);
@@ -7,16 +15,18 @@ function FindDistance() {
   const [endLoc, setEndLoc] = useState("");
   const [dist, setDist] = useState(0);
 
-  //   const getLatAndLon = (location) => {
-  //     return fetch(
-  //       `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?proximity=ip&access_token=pk.eyJ1IjoibWFuYXZsYWw1IiwiYSI6ImNsanRnejg5MTB2enEzZ282aGUzaW5sc2QifQ.4ityPQihWBgLs2DG4LMUGQ`
-  //     )
-  //       .then((res) => res.json())
-  //       .then((data) => ({
-  //         lat: data[0].features[0].center[0],
-  //         long: data[0].features[0].center[1],
-  //       }));
-  //   };
+  const style = {
+    position: "absolute",
+
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   const getLatAndLon = (location) => {
     return fetch(
@@ -46,37 +56,61 @@ function FindDistance() {
     const startCoords = [startLat, startLong];
     const { lat: endLat, long: endLong } = await getLatAndLon(endLoc);
     const endCoords = [endLat, endLong];
-    setDist(getDistance(startCoords, endCoords));
+    setDist(await getDistance(startCoords, endCoords));
     console.log(dist);
   };
 
   return (
     <div>
-      <Card variant="outlined">
-        <input
-          type="text"
-          id="message"
-          name="message"
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Starting Location
+        </Typography>
+
+        <br />
+        <br />
+
+        <TextField
+          id="outlined-basic"
+          label="Start Location"
+          variant="outlined"
           onChange={(event) => {
             setStartLoc(event.target.value);
           }}
           value={startLoc}
+          style={{ width: "20em" }}
         />
-        <input
-          type="text"
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Ending Location
+        </Typography>
+        <TextField
+          id="outlined-basic"
+          label="End Location"
+          variant="outlined"
           onChange={(event) => {
             setEndLoc(event.target.value);
           }}
           value={endLoc}
+          style={{ width: "20em" }}
         />
-        <button
+        <br />
+        <br />
+
+        <Button
+          variant="outlined"
           onClick={() => {
             handleSearch();
           }}
         >
-          search
-        </button>
-      </Card>
+          Find Distance
+        </Button>
+        <br />
+        <br />
+
+        <Divider />
+        <ListItemText primary="Distance" />
+        <h1>{(dist.distance.toString() / 1000).toFixed(2)} KM</h1>
+      </Box>
     </div>
   );
 }
