b596a117e31e1d378121edafa41d36deda1d8025
Manav Lal <manavlal5@gmail.com>
2023-07-09 12:28:38 +1200

    global state bare bones implemented

---
+++ src/GlobalState.js
@@ -0,0 +1,26 @@
+import React, { createContext, useState } from "react";
+
+export const GlobalStateContext = createContext();
+
+export const GlobalStateProvider = ({ children }) => {
+  const [distTravelled, setDistTravelled] = useState(0);
+  const [petChosen, setPetChosen] = useState(null);
+  const [playerName, setPlayerName] = useState("");
+  const [petName, setPetName] = useState("");
+  return (
+    <GlobalStateContext.Provider
+      value={{
+        distTravelled,
+        setDistTravelled,
+        petChosen,
+        setPetChosen,
+        playerName,
+        setPlayerName,
+        petName,
+        setPetName,
+      }}
+    >
+      {children}
+    </GlobalStateContext.Provider>
+  );
+};
